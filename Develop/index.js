7693720d0116b375bc4dae3121548f87fd95d780
Jennifer Arboleda <arboleda.jen@gmail.com>
2022-11-29 20:05:29 -0600

    updated question prompts, wrote function to write README file. Created function to generate markdown for README.

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,72 @@
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer');
-const fs = require('fs');
+const fs = require ('fs');
+const util = require ('util')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require ("./util/generateMarkdown.js");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is should we title your README?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What is your license type?',
+            choices: ['GNU AGPLv3', 'Apache License 2.0', 'MIT', 'The Unlicense'],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'List your collaborators, if any (with links to their GitHub profiles):',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide examples on how to any tests here:',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+    .then((data) => {
+        const content = generateMarkdown(data);
+        fs.writeFile('gen-ReadMe.md', content, (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
---
+++ Develop/util/generateMarkdown.js
@@ -0,0 +1,51 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+//READEME STRUCTURE: title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+ 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributions
+  ${data.contributing}
+
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For inquiries, please email me at ${data.email} or visit my [GitHub](https://github.com/${data.github})
+`;
+}
+
+module.exports = generateMarkdown;
